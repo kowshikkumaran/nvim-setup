c588b9815070d2b5226ff6ae05adbfc8788b27ae
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-01 01:47:01 +0530

    just a little tweak

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jj", "<esc>", { noremap = true })
-vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true, silent = true })
-
 local function run_code()
   local ft = vim.bo.filetype
   local cmd = ""
@@ -44,3 +41,6 @@ local function run_code()
 end
 
 vim.keymap.set("n", "<leader>r", run_code, { desc = "Run current file" })
+
+vim.keymap.set("i", "jj", "<esc>", { noremap = true })
+vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true, silent = true })
