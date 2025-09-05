27bf8e22720a548cc601f14adb6ac381ad558e42
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-05 23:53:23 +0530

    java class not found issue fixed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
+-- File: lua/config/keymaps.lua (your file)
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
+-- =========================
+-- Run Code per Filetype
+-- =========================
 local function run_code()
   local ft = vim.bo.filetype
   local cmd = ""
@@ -9,11 +11,19 @@ local function run_code()
   if ft == "python" then
     cmd = "py -3.11 " .. vim.fn.shellescape(vim.fn.expand("%"))
   elseif ft == "java" then
-    cmd = "javac " .. vim.fn.shellescape(vim.fn.expand("%")) .. " && java " .. vim.fn.shellescape(vim.fn.expand("%:r"))
+    local filename = vim.fn.expand("%:t") -- e.g. Main.java
+    local classname = vim.fn.expand("%:t:r") -- e.g. Main (without .java)
+    local filepath = vim.fn.expand("%:p") -- full path
+
+    -- Compile to the same directory
+    cmd = "javac "
+      .. vim.fn.shellescape(filepath)
+      .. " && java -cp "
+      .. vim.fn.shellescape(vim.fn.expand("%:p:h"))
+      .. " "
+      .. classname
   elseif ft == "javascript" then
     cmd = "node " .. vim.fn.shellescape(vim.fn.expand("%"))
-  elseif ft == "html" then
-    cmd = "start " .. vim.fn.shellescape(vim.fn.expand("%"))
   elseif ft == "c" then
     cmd = "gcc "
       .. vim.fn.shellescape(vim.fn.expand("%"))
@@ -42,30 +52,14 @@ end
 
 vim.keymap.set("n", "<leader>r", run_code, { desc = "Run current file" })
 
-vim.keymap.set("i", "jj", "<esc>", { noremap = true })
-vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true, silent = true })
+-- =========================
+-- Escape mappings
+-- =========================
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
+vim.keymap.set("t", "jj", [[<C-\><C-n>]], { noremap = true, silent = true })
 
-return {
-  "LazyVim/LazyVim",
-  opts = function(_, opts)
-    local map = vim.keymap.set
-    local opts = { noremap = true, silent = true }
-
-    --  Normal mode basics
-    map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
-    map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit window" })
-
-    --  Terminal mode basics
-    -- Escape to Normal mode
-    map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-
-    -- Window navigation from terminal
-    map("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Go left" })
-    map("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Go down" })
-    map("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Go up" })
-    map("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Go right" })
-
-    map("n", "<leader>tt", "<cmd>terminal<cr>", { desc = "New terminal" })
-    map("t", "<CR>", "<CR>", { desc = "Send Enter inside terminal" })
-  end,
-}
+-- =========================
+-- Terminal mappings
+-- =========================
+vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
+vim.keymap.set("t", "<leader>tt", [[<C-\><C-n><cmd>ToggleTerm<cr>]], { desc = "Toggle terminal" })
