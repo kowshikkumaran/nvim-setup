4f6095dda477ee384d0f577ddcd7670bcb44a4ac
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2025-09-09 15:00:21 +0530

    toggle current dir fix

--- lua/plugins/ToggleTerm.lua
+++ lua/plugins/ToggleTerm.lua
@@ -4,15 +4,23 @@ return {
   config = function()
     require("toggleterm").setup({
       size = 15,
-      open_mapping = [[<C-/>]], -- LazyVim’s default
-      hide_numbers = true,
+      open_mapping = [[<C-/>]],
       shade_terminals = true,
       start_in_insert = true,
-      insert_mappings = true,
       persist_size = true,
-      direction = "float", -- options: 'float' | 'horizontal' | 'vertical' | 'tab'
+      direction = "float",
       close_on_exit = true,
       shell = vim.o.shell,
+    })
+
+    -- Auto change cwd to the current file’s directory
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if bufname ~= "" then
+          vim.cmd("cd " .. vim.fn.fnamemodify(bufname, ":p:h"))
+        end
+      end,
     })
   end,
 }
