f620d99965f487ee0e239b2e383fc88a87653b5f
kowshik kumaran <kowshikkumaran.s12345@gmail.com>
2026-05-21 23:43:53 +0530

    Simplify Java compilation command in keymaps.lua
    Removed JavaFX specific compilation commands for Java.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,18 +24,7 @@ local function run_code()
       end
     end
 
-    if uses_javafx then
-      local javafx_path = "C:/Users/91994/Downloads/openjfx-24.0.2_windows-x64_bin-sdk/javafx-sdk-24.0.2/lib"
-
-      cmd = "javac --module-path "
-        .. vim.fn.shellescape(javafx_path)
-        .. " --add-modules=javafx.controls -d out "
-        .. vim.fn.shellescape(filepath)
-        .. " && java --module-path "
-        .. vim.fn.shellescape(javafx_path)
-        .. " --add-modules=javafx.controls -cp out "
-        .. classname
-    else
+    if ft == "java" then
       cmd = "javac " .. vim.fn.shellescape(filepath) .. " && java " .. classname
     end
   elseif ft == "javascript" then
